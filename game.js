690152a2c10d676031ea691324439d5866480fdb
nazbur <89898027+nazbur@users.noreply.github.com>
2021-11-23 18:03:31 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.112.1/build/three.module.js';
 import {WEBGL} from 'https://cdn.jsdelivr.net/npm/three@0.112.1/examples/jsm/WebGL.js';
-import {graphics} from './graphics.js';
+import {graphics} from 'graphics.js';
 
 export const game = (function() {
   return {
